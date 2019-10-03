9d7f37a4a0af78677e605c10e1b5c98c442dfdb5
sam <samuelmovi@protonmail.com>
2019-10-03 13:24:27 +0200

    more controller tests

--- src/test/java/samuelmovi/familyLibraryJava/controller/ControllerTest.java
+++ src/test/java/samuelmovi/familyLibraryJava/controller/ControllerTest.java
@@ -46,6 +46,38 @@ public class ControllerTest {
         Assert.assertEquals(view.booksColor, view.getBooksB().getBackground());
         Assert.assertEquals(Color.WHITE, view.getLocationsB().getBackground());
         Assert.assertEquals(Color.WHITE, view.getLoansB().getBackground());
+    }
+
+    @Test
+    public void showLocationsBTest(){
+        // execute showLocationsB()
+        controller.showLocationsB();
+        // assert only visible panel is BooksPanel
+        Assert.assertTrue(view.getLocationsPanel().isVisible());
+        Assert.assertFalse(view.getLoansPanel().isVisible());
+        Assert.assertFalse(view.getBooksPanel().isVisible());
+        Assert.assertFalse(view.getHomePanel().isVisible());
+
+        // assert buttons' background colors are properly set
+        Assert.assertEquals(view.locationsColor, view.getLocationsB().getBackground());
+        Assert.assertEquals(Color.WHITE, view.getBooksB().getBackground());
+        Assert.assertEquals(Color.WHITE, view.getLoansB().getBackground());
+    }
+
+    @Test
+    public void showLoansBTest(){
+        // execute showLoansB()
+        controller.showLoansB();
+        // assert only visible panel is BooksPanel
+        Assert.assertTrue(view.getLoansPanel().isVisible());
+        Assert.assertFalse(view.getBooksPanel().isVisible());
+        Assert.assertFalse(view.getLocationsPanel().isVisible());
+        Assert.assertFalse(view.getHomePanel().isVisible());
+
+        // assert buttons' background colors are properly set
+        Assert.assertEquals(view.loansColor, view.getLoansB().getBackground());
+        Assert.assertEquals(Color.WHITE, view.getLocationsB().getBackground());
+        Assert.assertEquals(Color.WHITE, view.getBooksB().getBackground());
 
     }
 
